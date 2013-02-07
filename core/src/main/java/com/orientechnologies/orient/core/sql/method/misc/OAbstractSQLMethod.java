56e50d0e9c89174d9489c797133c18796e1e750a
Johann Sorel <johann.sorel@geomatys.com>
2013-02-07 13:24:12 +0100

    SQLParser : use registered methods

--- core/src/main/java/com/orientechnologies/orient/core/sql/method/misc/OAbstractSQLMethod.java
+++ core/src/main/java/com/orientechnologies/orient/core/sql/method/misc/OAbstractSQLMethod.java
@@ -15,19 +15,16 @@
  */
 package com.orientechnologies.orient.core.sql.method.misc;
 
-import com.orientechnologies.orient.core.db.record.OIdentifiable;
-import com.orientechnologies.orient.core.record.impl.ODocument;
 import com.orientechnologies.orient.core.sql.method.OSQLMethod;
 
 /**
  * 
  * @author Johann Sorel (Geomatys)
  */
-public abstract class OAbstractSQLMethod implements OSQLMethod {
+public abstract class OAbstractSQLMethod extends OSQLMethod {
 
-  private final String name;
-  private final int    minparams;
-  private final int    maxparams;
+  private final int minparams;
+  private final int maxparams;
 
   public OAbstractSQLMethod(String name) {
     this(name, 0);
@@ -38,14 +35,9 @@ public abstract class OAbstractSQLMethod implements OSQLMethod {
   }
 
   public OAbstractSQLMethod(String name, int minparams, int maxparams) {
-    this.name = name;
+    super(name, null);
     this.minparams = minparams;
     this.maxparams = maxparams;
-  }
-
-  @Override
-  public String getName() {
-    return name;
   }
 
   @Override
@@ -86,28 +78,7 @@ public abstract class OAbstractSQLMethod implements OSQLMethod {
     return maxparams;
   }
 
-  protected Object getParameterValue(final OIdentifiable iRecord, final String iValue) {
-    if (iValue == null) {
-      return null;
-    }
-
-    if (iValue.charAt(0) == '\'' || iValue.charAt(0) == '"') {
-      // GET THE VALUE AS STRING
-      return iValue.substring(1, iValue.length() - 1);
-    }
-
-    // SEARCH FOR FIELD
-    return ((ODocument) iRecord.getRecord()).field(iValue);
-  }
-
-  @Override
   public int compareTo(OSQLMethod o) {
     return this.getName().compareTo(o.getName());
   }
-
-  @Override
-  public String toString() {
-    return name;
-  }
-
 }
